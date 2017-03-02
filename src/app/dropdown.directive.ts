88ba7b8ccdfe6b52a8381982170ef32acc044f9a
Breno Caldeira <breno.caldeira@gmail.com>
2017-03-02 11:30:38 -0300

    Directive - creates HostBinding and HostListener for Dropdown

--- src/app/dropdown.directive.ts
+++ src/app/dropdown.directive.ts
@@ -1,8 +1,19 @@
-import { Directive } from '@angular/core';
+import { Directive, HostBinding, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[rbDropdown]'
 })
 export class DropdownDirective {
+  @HostBinding('class.open') get opened() {
+    return this.isOpen;
+  }
+  @HostListener('click') open() {
+    // this.isOpen = !this.isOpen;
+    this.isOpen = true;
+  }
+  @HostListener('mouseleave') algo() {
+    this.isOpen = false;
+  }
 
+private isOpen = false;
 }
