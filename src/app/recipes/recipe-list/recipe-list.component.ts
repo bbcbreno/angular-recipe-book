9cbb76e60211a927aa6ee64d1cff15f0af03c5b4
Breno Caldeira <breno.caldeira@gmail.com>
2017-03-02 03:42:16 -0300

    adds ingredient class with | **cmd** ng g cl ingredient

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,8 +8,8 @@ import { Recipe } from '../recipe';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('John', 'Bel', 'http://orig00.deviantart.net/edfb/f/2013/025/0/7/pocket_john_watson_by_hotaruxsasuke1233-d5sqk8i.png'),
-    new Recipe('Anna', 'Okay', 'http://orig09.deviantart.net/d55b/f/2012/201/8/c/mine_turtle_id_by_hotaruxsasuke1233-d57ypfr.png')
+    new Recipe('John', 'Bel', 'http://orig00.deviantart.net/edfb/f/2013/025/0/7/pocket_john_watson_by_hotaruxsasuke1233-d5sqk8i.png', []),
+    new Recipe('Anna', 'Okay', 'http://orig09.deviantart.net/d55b/f/2012/201/8/c/mine_turtle_id_by_hotaruxsasuke1233-d57ypfr.png', [])
   ];
   @Output() recipeSelected = new EventEmitter<Recipe>();
 
