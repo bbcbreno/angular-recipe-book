6cc2000c9b7ef4168b5d6136b8baa83d71f60ecf
Breno Caldeira <breno.caldeira@gmail.com>
2017-03-03 10:33:25 -0300

    adds shopping-list service into recipe-detail

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 import { Recipe } from '../recipe';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Component({
   selector: 'rb-recipe-detail',
@@ -9,10 +10,14 @@ import { Recipe } from '../recipe';
 export class RecipeDetailComponent implements OnInit {
   @Input() selectedRecipe: Recipe;
 
-  constructor() { }
+  constructor(private sls: ShoppingListService) { }
 
   ngOnInit() {
     // this.selectedRecipe = new Recipe('jDummy', 'jDumy', 'http://4.bp.blogspot.com/-RBuRPj2WQJs/TbgVeyN2TQI/AAAAAAAAAoY/eUD7kVx7Kjk/s1600/tracejado.jpg');
+  }
+
+  onAddToShoppingList() {
+    this.sls.addItems(this.selectedRecipe.ingredients);
   }
 
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@angular/core';
 
 import { Recipe } from './recipe';
+import { Ingredient } from './ingredient';
 
 @Injectable()
 export class RecipeService {
   private recipes: Recipe[] = [
-    new Recipe('John', 'Bel', 'http://orig00.deviantart.net/edfb/f/2013/025/0/7/pocket_john_watson_by_hotaruxsasuke1233-d5sqk8i.png', []),
+    new Recipe('John', 'Bel', 'http://orig00.deviantart.net/edfb/f/2013/025/0/7/pocket_john_watson_by_hotaruxsasuke1233-d5sqk8i.png', [
+      new Ingredient('Batatas', 2),
+      new Ingredient('Coca', 1)
+    ]),
     new Recipe('Anna', 'Okay', 'http://orig09.deviantart.net/d55b/f/2012/201/8/c/mine_turtle_id_by_hotaruxsasuke1233-d57ypfr.png', [])
   ];
 
